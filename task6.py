8a6766b4c3ea78c5beafa683f674ff0949dc8358
Егор Базанов <egorbazanow@bk.ru>
2024-10-14 14:10:50 +0700

    почти все доделал

--- task6.py
+++ task6.py
@@ -1,3 +1,14 @@
 # с клавиатуры вводятся числа, ввод завершается числом 0.
 # Определить минимальное и максимальное из введённых чисел.
+a = int(input())
+min = a
+max = a
+while a !=0:
+    if max>a:
+        max = a
+    elif min < a:
+        min = a
+    a = int(input())
+print(max, min)
+
 
--- task9.py
+++ task9.py
@@ -1,4 +1,22 @@
 # с клавиатуры вводится число N, а затем – N целых чисел.
 # Определить минимальное и максимальное среди двузначных чисел,
 # которые делятся на 3. Если таких чисел не было, вывести "нет".
-
+n =int(input())
+N = int(input())
+min = n
+max = n
+k = 0
+for i in range(0,N):
+    x = int(input())
+    if x >= 10 and x <= 99:
+        if min<x:
+            min = x
+        elif max > x:
+            max = x
+        elif x == 0:
+            break
+        k += 1
+if k !=0:
+    print(min, max)
+else:
+    print('no')
